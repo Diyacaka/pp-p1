e89bce15f725ea20fefb7bd9a9aeb26d51b629cd
Ridho Rambe <msyrre29@gmail.com>
2024-05-22 18:03:02 +0700

    migration database

---
+++ migrations/20240522110016-Add-fk-to-transaction.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.addColumn('Transactions', 'UserProfileId', {
+      type : Sequelize.INTEGER,
+      references : {
+        model : 'UserProfiles',
+        key : 'id'
+      }, 
+      onDelete : 'cascade',
+      onUpdate : 'cascade'
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    queryInterface.removeColumn('Transactions', 'UserProfileId')
+  }
+};
